cfe19e13a18cc32e52715ff87536213094f12781
Augusto <jesus_augga_99@icloud.com>
2022-04-19 12:55:35 -0500

    refactor calculator to class

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,38 @@
 import React from 'react';
 
-const Calculator = () => {
-  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-  const setClass = (button) => {
-    if (button === '÷' || button === 'x' || button === '-' || button === '+' || button === '=') {
-      return 'calc-button orange-button';
-    }
+const setClass = (button) => {
+  if (button === '÷' || button === 'x' || button === '-' || button === '+' || button === '=') {
+    return 'calc-button orange-button';
+  }
 
-    if (button === '0') {
-      return 'calc-button double-sized';
-    }
+  if (button === '0') {
+    return 'calc-button double-sized';
+  }
 
-    return 'calc-button';
-  };
+  return 'calc-button';
+};
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  return (
-    <div className="calculator-container">
-      <div className="calculator">
-        <div className="calc-screen">
-          <p>0</p>
-        </div>
-        <div className="buttons">
-          {buttons.map((button) => (<button key={button} className={setClass(button)} type="button">{button}</button>))}
+  render() {
+    return (
+      <div className="calculator-container">
+        <div className="calculator">
+          <div className="calc-screen">
+            <p>0</p>
+          </div>
+          <div className="buttons">
+            {buttons.map((button) => (<button key={button} className={setClass(button)} type="button">{button}</button>))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Calculator;
