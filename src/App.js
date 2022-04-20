c950f62a2508680002260c7e8690c8cd77208e4f
Augusto <jesus_augga_99@icloud.com>
2022-04-20 15:31:32 -0500

    refactor app to component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import './App.scss';
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          Calculator
-        </header>
-        <div>
-          <Calculator />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      Calculator
+    </header>
+    <div>
+      <Calculator />
+    </div>
+  </div>
+);
 
 export default App;
