c37c8cb5db3befcaab6ea57ec53a0ab53e225294
Augusto <jesus_augga_99@icloud.com>
2022-04-26 12:10:22 -0500

    add content to quote

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import styles from './Quote.module.scss';
 
-const Quote = () => {
-  const title = 'Quote';
-  return (
-    <header>
-      <h2>{title}</h2>
-    </header>
-  );
-};
+const Quote = () => (
+  <div className={styles.quoteContainer}>
+    <p className={styles.quote}>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      It is about understanding.
+    </p>
+    <p className={styles.author}>- William Paul Thurston</p>
+  </div>
+);
 
 export default Quote;
